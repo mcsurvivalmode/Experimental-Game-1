6e0f84878030e4c0e90224e4b9b6bb28b7015341
mcsurvivalmode <C24385051@mytudublin.ie>
2026-02-04 13:36:11 +0000

    spirit moving around using path

---
+++ Experimental_Game1/Assets/scripts/path.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Drawing;
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class path : MonoBehaviour
+{
+    [SerializeField]Transform[] Point;
+    [SerializeField]private float moveSpeed;
+    private int pointIndex;
+
+    void Start()
+    {
+        transform.position = Point[pointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pointIndex <= Point.Length - 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Point[pointIndex].transform.position, moveSpeed * Time.deltaTime);
+
+            if(transform.position == Point[pointIndex].transform.position)
+            {
+                pointIndex += 1;
+            }
+
+            if (pointIndex == Point.Length)
+            {
+                pointIndex = 0;
+            }
+        }
+    }
+}
