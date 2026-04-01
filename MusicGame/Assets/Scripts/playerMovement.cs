8f3918dd2b7549bce598cf5fd4c8fccb4612d9fa
mcsurvivalmode <C24385051@mytudublin.ie>
2026-04-01 16:33:19 +0100

    player movement

---
+++ MusicGame/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    
+    public float moveSpeed;
+    float speedX, speedY;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+   
+    void Update()
+    {
+        speedX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * moveSpeed;
+        rb.linearVelocity = new Vector2(speedX, speedY);
+    }
+}
