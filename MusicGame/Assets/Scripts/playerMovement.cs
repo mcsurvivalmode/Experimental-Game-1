64e0d047c6e693ef9504da6978061c8fdea9e207
mcsurvivalmode <C24385051@mytudublin.ie>
2026-04-02 17:07:06 +0100

    added health and death working yay and added some lights they are kinds weird
    i also added the warning grey boxes for the white

--- MusicGame/Assets/Scripts/playerMovement.cs
+++ MusicGame/Assets/Scripts/playerMovement.cs
@@ -1,15 +1,23 @@
+using Mono.Cecil.Cil;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class playerMovement : MonoBehaviour
 {
     
     public float moveSpeed;
     float speedX, speedY;
+    int Health = 5;
+    public string sceneToLoad;
+
+    public TMP_Text healthText;
     Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+     
     }
 
    
@@ -18,5 +26,19 @@ public class playerMovement : MonoBehaviour
         speedX = Input.GetAxisRaw("Horizontal") * moveSpeed;
         speedY = Input.GetAxisRaw("Vertical") * moveSpeed;
         rb.linearVelocity = new Vector2(speedX, speedY);
+
+        if (Health ==0)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+
+
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        Health--;
+        Debug.Log("HIT");
+        healthText.text = "Health: " + Health.ToString ();
+
     }
 }
