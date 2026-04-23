174da56749b38ce995713c3a7ad3250995e730a7
Ava-TU <C24385051@mytudublin.ie>
2026-04-23 23:58:38 +0100

    objects speed up over time

---
+++ Educational game/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public GameObject[] spawnPoints;
+    public float timer;
+    public float timeBetweenSp;
+
+    public float speedMultiplier; 
+
+    public GameObject spawnObject;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        speedMultiplier += Time.deltaTime * 0.1f; 
+
+        timer += Time.deltaTime;
+
+        if(timer > timeBetweenSp)
+        {
+            timer = 0;
+            int randNum = Random.Range(0, 3);
+            Instantiate(spawnObject, spawnPoints[randNum].transform.position, Quaternion.identity);
+
+        }
+    }
+}
--- Educational game/Assets/Scripts/spawnObjectScript.cs
+++ Educational game/Assets/Scripts/spawnObjectScript.cs
@@ -4,14 +4,21 @@ public class spawnObjectScript : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float speed;
+
+    private GameManagerScript gm;
     void Start()
     {
+        gm = GameObject.FindGameObjectWithTag("GameManagerScript").GetComponent<GameManagerScript>();
+
         rb = GetComponent<Rigidbody2D>();
     }
 
  
     void Update()
     {
-        rb.linearVelocity = Vector2.left * speed; 
+
+
+
+        rb.linearVelocity = Vector2.left * (speed * gm.speedMultiplier); 
     }
 }
